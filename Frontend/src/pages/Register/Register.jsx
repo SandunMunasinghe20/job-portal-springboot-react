3eacbfd150da6ab108dabf8d52fec7bdcab227f2
SandunMunasinghe <msandunlakshan2001@gmail.com>
2025-06-24 19:11:03 +0530

    Implement Role select while registering

--- Frontend/src/pages/Register/Register.jsx
+++ Frontend/src/pages/Register/Register.jsx
@@ -8,6 +8,9 @@ export default function Register(){
     const [password, setPassword] = useState("");
     const [err, setError] = useState("");
     const [success, setSuccess] = useState("");
+    const [selected,setSelected]=useState('seeker');
+
+    let url="";
 
     const handlesubmit = async (e) => {
         e.preventDefault();
@@ -16,17 +19,25 @@ export default function Register(){
         setSuccess("")
 
         try {
-            const response = await fetch("http://localhost:8080/api/auth/register-employer",{
+            if(selected=='seeker'){
+                url = "http://localhost:8080/api/auth/register-seeker"
+            }else{
+                url = "http://localhost:8080/api/auth/register-employer"
+            }
+            const response = await fetch(url,{
                 method:'POST',
                 headers:{
                     "Content-Type":"application/json"
                 },
                 body: JSON.stringify({email, password}),
+                    
             });
-
+            
+            console.log("data sending back is: ",{email,password});
             // get resp entity string msg
             const msg = await response.text();
-            console.log(response);
+            console.log(msg);
+
             if(!response.ok){
                 setError(msg);
                 return;
@@ -53,6 +64,25 @@ export default function Register(){
                 <div className="input-group">
                     <GetInput type="password" placeholder="Password" value={password} onChange={setPassword} />
                 </div>
+
+                <div className="toggle-buttons">
+                <button
+                    type='button'
+                    className={`role-btn ${selected === 'employer' ? 'active' : ''}`}
+                    onClick={() => setSelected('employer')}
+                >
+                    Employer
+                </button>
+                <button
+                    type='button'
+                    className={`role-btn ${selected === 'seeker' ? 'active' : ''}`}
+                    onClick={() => setSelected('seeker')}
+                >
+                    Job Seeker
+                </button>
+                </div>
+
+
                 <SubmitButton msg="Register" />
                 
                 {err && <p className="register-message register-error">{err}</p>}
