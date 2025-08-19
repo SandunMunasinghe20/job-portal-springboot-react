0cf604c0d1bb3ac84cb7ae891e5150d970436fc6
SandunMunasinghe20 <msandunlakshan2001@gmail.com>
2025-08-19 12:24:48 +0530

    Configured H2 in-memory DB for tests and fixed ApplicationContext issue

--- Backend/src/test/java/com/example/job/portal/JobPortalApplicationTests.java
+++ Backend/src/test/java/com/example/job/portal/JobPortalApplicationTests.java
@@ -3,12 +3,15 @@ package com.example.job.portal;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JobPortalApplicationTests {
 
 	@Test
-	@Disabled("Disabled to unblock CI, fix ApplicationContext issue later")
+
+		//@Disabled("Disabled to unblock CI, fix ApplicationContext issue later")
 	void contextLoads() {
 	}
 
